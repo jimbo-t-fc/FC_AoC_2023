c1b692e87b45fa69f6e8524aef753ea674c68356
sgantayet <shubham.g.gantayet@pwc.com>
2023-12-01 12:55:51 +0000

    feat: SG enters the arena 🥊

---
+++ SG/main.py
@@ -0,0 +1,5 @@
+import solutions.day_1
+
+if __name__ == "__main__":
+    solutions.day_1.main("input", "output")
+    
---
+++ SG/solutions/day_1.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+
+
+class CalibrationUnscrambler:
+    number_string_lookup = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+
+    def __init__(self, calibration_file: str) -> None:
+        self.calibration_file = Path(calibration_file)
+
+    def unscramble_calibration_file(
+        self, output_file: str, convert_number_strings: bool = False
+    ):
+        output_lines = []
+        output_path = Path(output_file)
+
+        with open(self.calibration_file, "r") as f:
+            for calibration_line in f.readlines():
+                if convert_number_strings:
+                    for k, v in self.number_string_lookup.items():
+                        calibration_line = calibration_line.replace(
+                            k, f"{k}{v}{k}"
+                        )  # prevents replacing parts of other numbers
+
+                first_num = last_num = None
+
+                # First number
+                for char in calibration_line:
+                    if char.isdigit():
+                        first_num = char
+                        break
+                else:
+                    continue  # no numbers found
+
+                # Last number
+                for char in calibration_line[::-1]:
+                    if char.isdigit():
+                        last_num = char
+                        break
+                else:
+                    continue  # no numbers found
+
+                output_lines.append(f"{first_num}{last_num}")
+
+        with open(output_path, "w") as o:
+            o.writelines(output_lines)
+
+        return sum(int(l) for l in output_lines)
+
+
+def main(input_dir="../input", output_dir="../output"):
+    unscrambler = CalibrationUnscrambler(Path(input_dir) / "day_1_input.txt")
+    sol = unscrambler.unscramble_calibration_file(
+        Path(output_dir) / "day_1_output_p1.txt"
+    )
+    print(sol)
+    sol2 = unscrambler.unscramble_calibration_file(
+        Path(output_dir) / "day_1_output_p2.txt", convert_number_strings=True
+    )
+    print(sol2)
+
+
+if __name__ == "__main__":
+    main()
