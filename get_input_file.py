d33985f5e0e1b296176ae5a0ac68d7dc8eaba7ee
jimbo-t-fc <152611893+jimbo-t-fc@users.noreply.github.com>
2023-12-01 11:54:08 +0000

    Delete get_input_file.py

--- get_input_file.py
+++
@@ -1,9 +0,0 @@
-def get_lines(day_number, level_number , test = False):
-    if test:
-        with open(f'input_files/aoc_day_{day_number}_test_input_{level_number}.txt') as file:
-            return file.read().split('\n')
-    else:
-        with open(f'input_files/aoc_day_{day_number}_input_{level_number}.txt') as file:
-            return file.read().split('\n')
-
-
