781b453a1afb2c0e57ca2c107690e7df6f61265b
sgantayet <shubham.g.gantayet@pwc.com>
2023-12-11 13:34:20 +0000

    day 10 😵 and day 11 💫

---
+++ SG/solutions/day_11.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from itertools import combinations
+
+
+def solution(input_dir, output_dir, p1):
+    input_file = Path(input_dir) / "day_11_input.txt"
+
+    with open(input_file, "r") as f:
+        data = f.read().split("\n")
+
+    ecs = set(range(len(data[0])))
+    ers = set(range(len(data)))
+    coords = []
+    for r, d in enumerate(data):
+        for c, l in enumerate(d):
+            if l == "#":
+                coords.append((r, c))
+                if c in ecs:
+                    ecs.remove(c)
+                if r in ers:
+                    ers.remove(r)
+
+    res = 0
+    for idx, c1 in enumerate(coords):
+        for c2 in coords[idx + 1 :]:
+            d = abs(c2[0] - c1[0]) + abs(c2[1] - c1[1])
+            for ec in ecs:
+                if (c1[1] < ec < c2[1]) or (c2[1] < ec < c1[1]):
+                    d += 1 if p1 else 999999
+            for er in ers:
+                if (c1[0] < er < c2[0]) or (c2[0] < er < c1[0]):
+                    d += 1 if p1 else 999999
+
+            res += d
+
+    return res
+
+
+def main(input_dir="../input", output_dir="../output"):
+    res1 = solution(input_dir, output_dir, True)
+    res2 = solution(input_dir, output_dir, False)
+    print("Part 1:", res1)
+    print("Part 2:", res2)
+
+
+if __name__ == "__main__":
+    main()
